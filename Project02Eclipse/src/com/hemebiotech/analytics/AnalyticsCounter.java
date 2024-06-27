d978080b798e3c52e5e51d2a83620846a0656d57
Dembs <demba.sy@hotmail.com>
2024-06-27 18:26:17 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,11 +3,44 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
+
+	ISymptomReader reader;
+	ISymptomWriter writer;
+
+	public AnalyticsCounter(ISymptomReader reader,ISymptomWriter writer){
+		this.reader = reader;
+		this.writer = writer;
+	}
+
+	public List<String> getSymptoms(){
+		return reader.GetSymptoms();
+	}
+
+	public Map<String,Integer>countSymptoms(List<String>symptoms){
+
+		Map<String,Integer> symptomCounts = new HashMap<>();
+
+		for (String symptom : symptoms) {
+			Integer j = symptomCounts.get(symptom);
+			symptomCounts.put(symptom, (j == null) ? 1 : j + 1);
+		}
+        return symptomCounts;
+    }
+
+	public Map<String,Integer>sortSymptoms(Map<String,Integer>symptoms){
+
+        return new HashMap<>(new TreeMap<>(symptoms));
+    }
+
+	public void writeSymptoms(Map<String,Integer>symptoms){
+		writer.writeSymptoms(symptoms);
+	}
 
 	public static void main(String args[]) throws Exception {
 
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -8,24 +8,25 @@ import java.io.IOException;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
     private String filepath;
+
     /**
-     *
      * @param filepath a full or partial path to file with symptom strings in it, one per line
      */
-    public WriteSymptomDataToFile (String filepath) {
+    public WriteSymptomDataToFile(String filepath) {
         this.filepath = filepath;
     }
+
     @Override
     public void writeSymptoms(Map<String, Integer> symptoms) {
-
-        try  {
-            FileWriter writer = new FileWriter(filepath);
-            for (Map.Entry<String, Integer> symptom : symptoms.entrySet()) {
-                writer.write(symptom.getKey() + " : " + symptom.getValue() + "\n");
+        if (filepath != null) {
+            try {
+                FileWriter writer = new FileWriter(filepath);
+                for (Map.Entry<String, Integer> symptom : symptoms.entrySet()) {
+                    writer.write(symptom.getKey() + " : " + symptom.getValue() + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
